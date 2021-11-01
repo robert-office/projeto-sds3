60809e9224dfed84fc334cfbcc909503ecbd93ba
robert-office <robertuillians123@gmail.com>
2021-11-01 17:50:13 -0300

    Adicionado o pacote do Bootstrap as dependencias do projeto

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import App from './App';
+import 'bootstrap/dist/css/bootstrap.css';
+import ReactDOM from 'react-dom';
+import "assets/css/styles.css";
 
 ReactDOM.render(
   <React.StrictMode>
