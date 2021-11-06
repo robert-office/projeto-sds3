5f550a96584f279312ebc3bc4b7d4dd533ee4717
robert-office <robertuillians123@gmail.com>
2021-11-06 11:40:31 -0300

    Group by search

--- backend/src/main/java/com/devsuperior/dsvendas/controllers/SellerController.java
+++ backend/src/main/java/com/devsuperior/dsvendas/controllers/SellerController.java
@@ -13,7 +13,7 @@ import com.devsuperior.dsvendas.services.SellerService;
 
 @RestController
 @RequestMapping(value = "/sellers")
-public class SellerConstroller {
+public class SellerController {
 	
 	@Autowired
 	private SellerService service;
